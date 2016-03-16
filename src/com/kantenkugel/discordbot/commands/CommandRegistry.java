1b5ce595dc0402c5c4a691463133e7d04b57ed05
Michael Ritter <ritter.michael92@gmail.com>
2016-03-16 12:10:19 +0100

    Improved formatting of clear command

--- src/com/kantenkugel/discordbot/commands/CommandRegistry.java
+++ src/com/kantenkugel/discordbot/commands/CommandRegistry.java
@@ -217,7 +217,7 @@ public class CommandRegistry extends ListenerAdapter {
                 MessageUtil.reply(m, "Incorrect time range!");
                 return;
             }
-            String msg = "Clearing messages younger " + args[1];
+            String msg = "Clearing messages younger " + args[1] + "... ";
             List<User> mentioned = m.getMessage().getMentionedUsers();
             if(!TaskHelper.start("clear" + m.getTextChannel().getId(), new ClearRunner(m.getTextChannel(), clearTo, mentioned, msg))) {
                 MessageUtil.reply(m, "There is already a clear-task running for this Channel!");
